cd8a70bb4567f44b8401eeee363e87361ef7604f
Faudel <hadroug.faudel@gmail.com>
2024-03-26 13:29:42 +0100

    feat: 404 catch router with view base

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
     {
       path: '/',
       name: 'home',
---
+++ client/src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section>
+    <h1>Page introuvable</h1>
+    <a href="/">Retournez à l'accueil</a>
+  </section>
+</template>
