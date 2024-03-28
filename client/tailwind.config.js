de83235b071a0f1dd0dd9114e37fc36a5259d683
Faudel <hadroug.faudel@gmail.com>
2024-03-28 16:47:01 +0100

    chore(style): add extra-color to the tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#20FFD6',
+        'primary-dark': '#0D6151',
+        'secondary': '#FF8360',
+        'secondary-dark': '#613225',
+        'dark': '#021815',
+        'dark-75': '#415250',
+        'dark-65': '#5B6967',
+        'grey': '#808C8A',
+        'white-35': '#A6AEAD',
+        'white-25': '#C0C5C4',
+        'white': '#F2F3F3',
+      },
+    },
   },
   plugins: [],
 }
