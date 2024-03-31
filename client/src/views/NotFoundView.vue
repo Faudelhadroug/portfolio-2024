c91a5f1234cc731d3720bc31dea76d17ed120977
Faudel <hadroug.faudel@gmail.com>
2024-03-31 19:26:28 +0200

    style: modification 404page

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <section>
+  <main class="flex-center flex-col h-screen space-y-[0.5rem]">
     <h1>Page introuvable</h1>
     <a href="/">Retournez à l'accueil</a>
-  </section>
+  </main>
 </template>
