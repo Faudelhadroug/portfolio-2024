9005c6d0ab028a73323fd54accf887009c61a4ca
Faudel <hadroug.faudel@gmail.com>
2024-04-02 23:58:42 +0200

    style: underline for redirection text

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -4,6 +4,6 @@
 <template>
   <main class="flex-center flex-col h-screen space-y-[0.5rem]">
     <h1>Page introuvable</h1>
-    <a href="/">Retournez à l'accueil</a>
+    <a class="underline" href="/">Retournez à l'accueil</a>
   </main>
 </template>
