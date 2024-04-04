af359b4b2e59faa25713df4b1eb69739c3b5eef4
Faudel <hadroug.faudel@gmail.com>
2024-04-04 05:14:23 +0200

    refactor: import view alias from router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ContactView from '../views/ContactView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ContactView from '@/views/ContactView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
