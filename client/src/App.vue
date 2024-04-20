65e08dca6ff2d2e7460f1617d8ac9ad72094120a
Faudel <hadroug.faudel@gmail.com>
2024-04-20 18:37:58 +0200

    style: add fade transition on App pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,5 +5,14 @@ import NavBar from '@/components/NavBar.vue'
 
 <template>
   <NavBar class="text-white" />
-  <RouterView />
+  <Transition
+    enter-active-class="duration-300 ease-out"
+    enter-from-class="transform opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="duration-200 ease-in"
+    leave-from-class="opacity-100"
+    leave-to-class="transform opacity-0"
+  >
+    <RouterView />
+  </Transition>
 </template>
