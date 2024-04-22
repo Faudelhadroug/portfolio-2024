45af02db87fffb5ca0a6fff43c41bdd7892d5a82
Faudel <hadroug.faudel@gmail.com>
2024-04-22 18:55:49 +0200

    chore: add unhead plugin into the app

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 import { defaultConfig, plugin } from '@formkit/vue'
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@unhead/vue'
 
 const app = createApp(App)
+const head = createHead()
 
+app.use(head)
 app.use(router)
 app.use(plugin, defaultConfig)
 app.mount('#app')
