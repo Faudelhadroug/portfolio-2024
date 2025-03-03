956a85f87577b68832e5813ddc62dc50ae4b2d57
Faudel <hadroug.faudel@gmail.com>
2025-03-03 03:13:16 +0100

    feat: maintenance update

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,10 @@ import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
-  <NavBar class="text-white" />
-  <RouterView />
+  <!-- <NavBar class="text-white" />
+  <RouterView /> -->
+  <div class="text-center pt-[10rem]">
+    <h1>Website en cours de maintenance</h1>
+    <p class="text-[30px] pt-8"><i>Retour du site prévu pour le mois de Mai</i></p>
+  </div>
 </template>
